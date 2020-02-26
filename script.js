72d319ef10b12ebb2eaf27c8d99c2db5522cedcc
corytimmons <43824202+corytimmons@users.noreply.github.com>
2020-02-25 21:19:48 -0800

    v2

---
+++ script.js
@@ -0,0 +1,17 @@
+	function hideTogglea() {
+  var x = document.getElementById("Event1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+	function hideToggleb() {
+  var x = document.getElementById("Event2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
